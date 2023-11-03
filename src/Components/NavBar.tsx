e4016640efb7db376df38515dd3a22e587992ab6
Oleksii-Kriuckov <75043251+Oleksii-Kriuckov@users.noreply.github.com>
2023-11-03 18:48:08 +0200

    made mobile navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,15 +1,30 @@
-import { MyOffcanvas } from './Offcanvas/Offcanvas'
-import GroupLinkButtons from './UI/Buttons/LinkButtons/GroupLinkButtons'
-import SearchInput from './UI/Search/SearchInput'
+import { MyOffcanvas } from "./Offcanvas/Offcanvas";
+import GroupLinkButtons from "./UI/Buttons/LinkButtons/GroupLinkButtons";
+import SearchInput from "./UI/Search/SearchInput";
 
 const NavBar = () => {
   return (
-    <div className='d-flex' style={{gap: 10}}>
-      <MyOffcanvas/>
-      <SearchInput/>
-      <GroupLinkButtons />
-    </div>
-  )
-}
+    <>
+      <div className="navbar">
+        <div className="d-flex" style={{ gap: 10 }}>
+          <MyOffcanvas />
+          <SearchInput />
+          <GroupLinkButtons />
+        </div>
+      </div>
 
-export default NavBar
+      <div className="navbar_mobile">
+        <div className="d-flex flex-column" style={{ gap: 10 }}>
+          <div className="d-flex justify-content-between" style={{ gap: 10 }}>
+            <MyOffcanvas />
+            <GroupLinkButtons />
+          </div>
+
+          <SearchInput />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
